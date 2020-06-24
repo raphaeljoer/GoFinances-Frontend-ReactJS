d176ca83156666094952b08068293e62a272e2fa
raphaeljoer <ra.joer@gmail.com>
2020-06-23 22:56:06 -0300

    list transactions and show balance

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import income from '../../assets/income.svg';
 import outcome from '../../assets/outcome.svg';
@@ -9,6 +9,7 @@ import api from '../../services/api';
 import Header from '../../components/Header';
 
 import formatValue from '../../utils/formatValue';
+import formatDate from '../../utils/formateDate';
 
 import { Container, CardContainer, Card, TableContainer } from './styles';
 
@@ -30,16 +31,48 @@ interface Balance {
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
+
+  const mountBalance = useCallback((dataBalance: Balance) => {
+    const newDataBalance: Balance = {
+      income: formatValue(Number(dataBalance.income)),
+      outcome: formatValue(Number(dataBalance.outcome)),
+      total: formatValue(Number(dataBalance.total)),
+    };
+
+    setBalance(newDataBalance);
+  }, []);
+
+  const mountTransactions = useCallback(dataTransactions => {
+    const newDataTransactions = dataTransactions.map(
+      (transaction: Transaction) => {
+        const newTransaction: Transaction = {
+          id: transaction.id,
+          title: transaction.title,
+          type: transaction.type,
+          value: transaction.value,
+          category: transaction.category,
+          formattedDate: formatDate(new Date(transaction.created_at)),
+          formattedValue: formatValue(transaction.value),
+          created_at: new Date(transaction.created_at),
+        };
+
+        return newTransaction;
+      },
+    );
+    setTransactions(newDataTransactions);
+  }, []);
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
+      const response = await api.get('/transactions');
+      mountBalance(response.data.balance);
+      mountTransactions(response.data.transactions);
     }
 
     loadTransactions();
-  }, []);
+  }, [mountBalance, mountTransactions]);
 
   return (
     <>
@@ -51,21 +84,21 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">{balance.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">{balance.total}</h1>
           </Card>
         </CardContainer>
 
@@ -81,18 +114,18 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction.title}</td>
+                  {transaction.type === 'income' ? (
+                    <td className="income">{transaction.formattedValue}</td>
+                  ) : (
+                    <td className="outcome">{`- ${transaction.formattedValue}`}</td>
+                  )}
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.formattedDate}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
